7361d468bf2d68d61ece83cdffca074d605731a6
Mihai Fufezan <fufexan@protonmail.com>
2022-08-11 17:05:44 +0300

    add desktopItem and icons

--- overrides/nodejs/default.nix
+++ overrides/nodejs/default.nix
@@ -5,12 +5,33 @@
   satisfiesSemver,
   ...
 }: {
-  webcord.runBuild = {
+  webcord.runBuild = let
+    desktopItem = pkgs.makeDesktopItem {
+      name = "WebCord";
+      desktopName = "WebCord";
+      genericName = "Discord and Fosscord client";
+      exec = "webcord";
+      icon = "webcord";
+      categories = ["Network" "InstantMessaging"];
+      mimeTypes = ["x-scheme-handler/discord"];
+    };
+  in {
     postBuild = ''
       mkdir code
       mv app/* code
       mv code app
       cp -r sources/translations app
+    '';
+
+    postInstall = ''
+      mkdir -p $out/share/icons/hicolor
+      for res in {24,48,64,128,256}; do
+        mkdir -p $out/share/icons/hicolor/''${res}x''${res}
+        ln -s $out/lib/node_modules/webcord/sources/assets/icons/app.png \
+          $out/share/icons/hicolor/''${res}x''${res}/webcord.png
+      done
+
+      ln -s "${desktopItem}/share/applications" $out/share/
     '';
 
     postFixup = ''
