6c6a355d1da59f729acb9042d8db423253b834bb
Mihai Fufezan <fufexan@protonmail.com>
2022-08-11 11:03:08 +0300

    Add overrides
    Co-authored-by: Yusuf Bera Ertan <y.bera003.06@protonmail.com>

--- overrides/nodejs/default.nix
+++ overrides/nodejs/default.nix
@@ -1,12 +1,21 @@
 {
   lib,
   pkgs,
-
   # dream2nix
   satisfiesSemver,
   ...
-}: 
+}: {
+  webcord.runBuild = {
+    postBuild = ''
+      mkdir code
+      mv app/* code
+      mv code app
+      cp -r sources/translations app
+    '';
 
-{
-
+    postFixup = ''
+      wrapProgram $out/bin/webcord \
+        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [pkgs.pipewire]}"
+    '';
+  };
 }
