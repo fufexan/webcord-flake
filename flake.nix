4cc220e94eeb61102c5051dad972c81e5efe7f46
Mihai Fufezan <fufexan@protonmail.com>
2022-08-11 11:42:53 +0300

    flake: add more systems

--- flake.nix
+++ flake.nix
@@ -1,30 +1,48 @@
 {
-  description = "WebCord";
+  description = "WebCord Nix Flake";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    dream2nix.url = "github:nix-community/dream2nix";
-    dream2nix.inputs.nixpkgs.follows = "nixpkgs";
+    dream2nix = {
+      url = "github:nix-community/dream2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     webcord = {
       url = "github:SpacingBat3/WebCord";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, dream2nix, webcord, ... }: let
-    system = "x86_64-linux";
-    dreamlib = dream2nix.lib.init {
-      pkgs = nixpkgs.legacyPackages.${system};
-      config = {
-        projectRoot = ./.;
-        overridesDirs = [ "${dream2nix}/overrides" ./overrides ];
-      };
-    };
-    dream = dreamlib.makeOutputs {
-      source = webcord;
-    };
+
+  outputs = {
+    self,
+    nixpkgs,
+    dream2nix,
+    webcord,
+    ...
+  }: let
+    supportedSystems = [
+      "aarch64-linux"
+      "x86_64-linux"
+
+      # open an issue if you want these
+      #"aarch64-darwin"
+      #"x86_64-darwin"
+    ];
+    genSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    dreamlib = genSystems (system:
+      dream2nix.lib.init {
+        pkgs = nixpkgs.legacyPackages.${system};
+        config = {
+          projectRoot = ./.;
+          overridesDirs = ["${dream2nix}/overrides" ./overrides];
+        };
+      });
+    dream = genSystems (system: dreamlib.${system}.makeOutputs {source = webcord;});
   in {
-    packages.${system} = rec {
-      inherit (dream.packages) webcord;
+    packages = genSystems (system: rec {
+      inherit (dream.${system}.packages) webcord;
       default = webcord;
-    };
+    });
   };
 }
